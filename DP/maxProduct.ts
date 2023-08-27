5a33b4fb6cec9b0b443fb49ea22e4c28c8eb7357
Mike Brinkman <mbrinkma13@gmail.com>
2023-08-27 16:35:11 -0400

    maxProduct DP

--- DP/maxProduct.ts
+++ DP/maxProduct.ts
@@ -1,3 +1,30 @@
+// DP
+function maxProductDP(nums: number[]): number {
+  // Initialize DP arrays and result variable
+  let n = nums.length;
+  let maxDp = new Array(n).fill(0);
+  let minDp = new Array(n).fill(0);
+  let res = nums[0];
+
+  // Base case
+  maxDp[0] = nums[0];
+  minDp[0] = nums[0];
+
+  // DP iteration
+  for (let i = 1; i < n; i++) {
+    // Calculate max and min product ending at position i
+    maxDp[i] = Math.max(nums[i], nums[i] * maxDp[i - 1], nums[i] * minDp[i - 1]);
+    minDp[i] = Math.min(nums[i], nums[i] * maxDp[i - 1], nums[i] * minDp[i - 1]);
+
+    // Update the result if we get a larger product
+    res = Math.max(res, maxDp[i]);
+  }
+
+  return res;
+}
+
+// greedy / iterative
+
 function maxProduct(nums: number[]): number {
   let res = Math.max(...nums);
 

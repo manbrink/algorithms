18b011ad60b1b22939173699160a165b350a96c3
Mike Brinkman <mbrinkma13@gmail.com>
2023-08-27 16:06:35 -0400

    longestPalindrome dp

--- DP/longestPalindrome.ts
+++ DP/longestPalindrome.ts
@@ -1,3 +1,43 @@
+// dp method
+function longestPalindrome(s: string): string {
+  let n = s.length;
+  let dp: boolean[][] = Array.from(Array(n), () => Array(n).fill(false));
+  let start = 0;  // The starting index of longest palindromic substring found so far
+  let maxLen = 1; // Length of longest palindromic substring found so far
+
+  // Every single character is a palindrome
+  for (let i = 0; i < n; ++i) {
+      dp[i][i] = true;
+  }
+
+  // Check two-character substrings
+  for (let i = 0; i < n - 1; ++i) {
+      if (s[i] == s[i + 1]) {
+          dp[i][i+1] = true;
+          start = i;
+          maxLen = 2;
+      }
+  }
+
+  // Check longer substrings
+  for (let len = 3; len <= n; ++len) {  // length of substring
+      for (let i = 0; i <= n - len; ++i) {
+          let j = i + len - 1;  // ending index of current substring
+          if (s[i] == s[j] && dp[i + 1][j - 1]) {  // check palindrome
+              dp[i][j] = true;
+              if (len > maxLen) {
+                  start = i;
+                  maxLen = len;
+              }
+          }
+      }
+  }
+
+  return s.substring(start, start + maxLen);
+}
+
+
+// center expansion method
 function longestPalindrome(s: string): string {
   let res = "";
   let resLen = 0;
--- DP/strategies.md
+++ DP/strategies.md
@@ -3,3 +3,5 @@ Recipe:
 1. Define the subproblem
 
 2. State the recursive relation
+
+- Can sometimes have tabular solutions to store previous work (longestPalindrome)
